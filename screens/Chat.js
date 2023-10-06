cd5966f290624d7ca11251fff37cf337dcc2300d
SaberFate01 <39643464+SaberFate01@users.noreply.github.com>
2023-10-06 11:41:46 +1000

    Added group chat.

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,28 +1,39 @@
 import React, {useContext} from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image} from 'react-native';
-import { useState, useEffect } from 'react';
-import { TextInput } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+} from 'react-native';
+import {useState, useEffect} from 'react';
+import {TextInput} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 import ChatPage from './ChatPage';
 import ChatGPT from './ChatGPT';
-import { UserContext } from '../userContext';
+import {UserContext} from '../userContext';
 
 const HomePage = () => {
   const [data, setData] = useState([]);
   const [usernames, setUsernames] = useState({});
   const navigation = useNavigation();
   const [searchTerm, setSearchTerm] = useState('');
-  const { user } = useContext(UserContext);
+  const {user} = useContext(UserContext);
   const [groupData, setGroupData] = useState([]);
 
-  const gptData = [{
-    otherID: '3',
-    usernames:'GPT',
-    rows: [{
-      message: 'Chat with GPT!',
-      timestamp: new Date().toISOString(),
-    }]
-  }];
+  const gptData = [
+    {
+      otherID: '3',
+      usernames: 'GPT',
+      rows: [
+        {
+          message: 'Chat with GPT!',
+          timestamp: new Date().toISOString(),
+        },
+      ],
+    },
+  ];
   const filteredData = data.filter(item => {
     const username = usernames[item.otherID] || 'Unknown User';
     return username.toLowerCase().includes(searchTerm.toLowerCase());
@@ -31,122 +42,136 @@ const HomePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/chat/get', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          'https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/chat/get',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              userID: user.userid,
+            }),
           },
-          body: JSON.stringify({
-            userID: user.userid,
-          }),
-        });
+        );
         const result = await response.json();
-        
+
         // Fetch usernames for all otherIDs
         const otherIDs = result.map(chatData => chatData.otherID);
         const usernames = await fetchUsernames(otherIDs);
-        
+
         // Add GPT to usernames
         usernames['3'] = 'GPT';
-        
+
         setUsernames(usernames);
-        
+
         // Merge gptData and fetched data
         setData([...gptData, ...result]);
       } catch (error) {
-        console.error("Error fetching chat data:", error);
+        console.error('Error fetching chat data:', error);
       }
     };
-  
+
     fetchData();
 
     const fetchGroupData = async () => {
       try {
-        const response = await fetch('https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/group_chat/get', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          'https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/group_chat/get',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              userID: user.userid,
+            }),
           },
-          body: JSON.stringify({
-            userID: user.userid,
-          }),
-        });
+        );
         const result = await response.json();
         setGroupData(result.inter_chat);
       } catch (error) {
-        console.error("Error fetching group chat data:", error);
+        console.error('Error fetching group chat data:', error);
       }
     };
 
     fetchGroupData();
-
   }, []);
 
-  const fetchUsernames = async (otherIDs) => {
+  const fetchUsernames = async otherIDs => {
     try {
       // Fetch usernames for all otherIDs and return an object mapping otherID to username
       const usernames = {};
       for (const otherID of otherIDs) {
-        const response = await fetch('https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/database', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          'https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/database',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              user: 'root',
+              pass: 'root',
+              db_name: 'users',
+              query: `SELECT * FROM userprofiles WHERE userid='${otherID}'`,
+            }),
           },
-          body: JSON.stringify({
-            user: 'root',
-            pass: 'root',
-            db_name: 'users',
-            query: `SELECT * FROM userprofiles WHERE userid='${otherID}'`,
-          }),
-        });
+        );
         const result = await response.json();
         usernames[otherID] = result[0]?.username || 'Unknown User';
       }
       return usernames;
     } catch (error) {
-      console.error("Error fetching usernames:", error);
+      console.error('Error fetching usernames:', error);
       return {};
     }
   };
-  const handlePersonalChatClick = (item) => {
+  const handlePersonalChatClick = item => {
     const username = usernames[item.otherID] || 'Unknown User';
     if (item.otherID === '3') {
-      navigation.navigate('ChatGPT', { chatData: item });
+      navigation.navigate('ChatGPT', {chatData: item});
     } else {
-      navigation.navigate('ChatPage', { chatData: item, username: username });
+      navigation.navigate('ChatPage', {chatData: item, username: username});
     }
   };
-  const handleGroupChatClick = (chatRoom) => {
-    navigation.navigate('ChatGroup', { chatRoom, userID: user.userid });
+  const handleGroupChatClick = chatRoom => {
+    console.log({chatRoom, userID: user.userid, username: user.username});
+    navigation.navigate('ChatPageGroup', {
+      chatRoom,
+      userID: user.userid,
+      username: user.username,
+    });
   };
 
   return (
     <View style={styles.container}>
-      <TextInput 
+      <TextInput
         style={styles.searchBar}
         placeholder="Search..."
         value={searchTerm}
         onChangeText={setSearchTerm}
       />
-      <Image 
+      <Image
         source={require('../assets/magnifying_glass.png')}
         style={styles.magnifyingGlass}
       />
       <FlatList
         data={[...data, ...groupData]}
-        keyExtractor={(item) => item.otherID || item.chatRoom.toString()}
-        renderItem={({ item }) => {
+        keyExtractor={item => item.otherID || item.chatRoom.toString()}
+        renderItem={({item}) => {
           if (item.otherID) {
             // Render personal chat
             const username = usernames[item.otherID] || 'Unknown User';
             const lastMessage = item.rows?.reduce((latest, message) => {
-              return new Date(message.timestamp) > new Date(latest.timestamp) ? message : latest;
-            }, item.rows[0]) || { message: 'No messages' };
+              return new Date(message.timestamp) > new Date(latest.timestamp)
+                ? message
+                : latest;
+            }, item.rows[0]) || {message: 'No messages'};
             return (
-              <TouchableOpacity 
+              <TouchableOpacity
                 style={styles.itemContainer}
-                onPress={() => handlePersonalChatClick(item)}
-              >
+                onPress={() => handlePersonalChatClick(item)}>
                 <View style={styles.logo}></View>
                 <View style={styles.textContainer}>
                   <Text style={styles.nameText}>{username}</Text>
@@ -157,13 +182,14 @@ const HomePage = () => {
           } else {
             // Render group chat
             const lastMessage = item.rows?.reduce((latest, message) => {
-              return new Date(message.timestamp) > new Date(latest.timestamp) ? message : latest;
-            }, item.rows[0]) || { message: 'No messages' };
+              return new Date(message.timestamp) > new Date(latest.timestamp)
+                ? message
+                : latest;
+            }, item.rows[0]) || {message: 'No messages'};
             return (
-              <TouchableOpacity 
+              <TouchableOpacity
                 style={styles.itemContainer}
-                onPress={() => handleGroupChatClick(item.chatRoom)}
-              >
+                onPress={() => handleGroupChatClick(item.chatRoom)}>
                 <View style={styles.logo}></View>
                 <View style={styles.textContainer}>
                   <Text style={styles.nameText}>{lastMessage.chatName}</Text>
--- screens/ChatPageGroup.js
+++ screens/ChatPageGroup.js
@@ -1,13 +1,234 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import React, {useState, useEffect, useContext} from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  Image,
+  Text,
+} from 'react-native';
+import {UserContext} from '../userContext';
+import axios from 'axios';
 
-const ChatPageGroup = () => {
+const ChatPageGroup = ({route, navigation}) => {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const {chatRoom, userID, username} = route.params;
+  const [groupData, setGroupData] = useState([]);
+  const {user} = useContext(UserContext);
+  //console.log(chatRoom,userID,username);
+
+  useEffect(() => {
+    // Fetch initial messages
+    fetchGroupMessage();
+
+    // Fetch messages every 5 seconds
+    const intervalId = setInterval(() => {
+      fetchGroupMessage();
+    }, 5000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const fetchGroupMessage = async () => {
+    try {
+      const response = await fetch(
+        'https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/group_chat/get',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            userID: user.userid,
+            chatRoom: chatRoom,
+          }),
+        },
+      );
+      const result = await response.json();
+      // Ensure that the messages are in the correct format and update the `messages` state
+      if (result.inter_chat && result.inter_chat.length > 0) {
+        setMessages(result.inter_chat[0].rows);
+      }
+    } catch (error) {
+      console.error('Error fetching group chat data:', error);
+    }
+  };
+
+  const formatTimestamp = date => {
+    const yyyy = date.getFullYear();
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const dd = String(date.getDate()).padStart(2, '0');
+    const hh = String(date.getHours()).padStart(2, '0');
+    const min = String(date.getMinutes()).padStart(2, '0');
+    const sec = String(date.getSeconds()).padStart(2, '0');
+    console.log(`${yyyy}/${mm}/${dd} ${hh}:${min}:${sec}`)
+    return `${yyyy}-${mm}-${dd} ${hh}:${min}:${sec}`;
+  };
+  const handleSubmit = async () => {
+    if (input) {
+      try {
+        // Send message to server
+        await axios.post(
+          'https://flask-dot-acoustic-cirrus-396009.ts.r.appspot.com/group_chat/send',
+          {
+            senderID: user.userid,
+            chatRoom: chatRoom,
+            chatName: "TestMessage",
+            timestamp: formatTimestamp(new Date()),
+            message: input,
+            type: "event", // Ensure this is correct for your use case
+            EventId: 1001, // Ensure this is correct for your use case
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+        // Add message to local state and clear input
+        setMessages(prevMessages => [
+          ...prevMessages,
+          {
+            senderID: user.userid,
+            chatRoom: chatRoom,
+            timestamp: new Date().toISOString(),
+            message: input,
+          },
+        ]);
+        setInput('');
+      } catch (error) {
+        console.error('Error sending message:', error);
+      }
+    }
+  };  
+
+  const BOT_IMG =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png';
+  const PERSON_IMG =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png';
+  const BOT_NAME = username;
+  const PERSON_NAME = user.username;
   return (
-    <WebView
-      source={{ uri: "acoustic-cirrus-396009.ts.r.appspot.com/" }}
-      style={{ flex: 1 }}
-    />
+    <View style={styles.container}>
+      <ScrollView style={styles.chat}>
+        {messages.map((msg, index) => (
+          <View
+            key={index}
+            style={
+              msg.senderID === user.userid ? styles.rightMsg : styles.leftMsg
+            }>
+            <Image
+              source={{
+                uri: msg.senderID === user.userid ? PERSON_IMG : BOT_IMG,
+              }}
+              style={styles.msgImg}
+            />
+            <View
+              style={
+                msg.senderID === user.userid
+                  ? styles.rightBubble
+                  : styles.leftBubble
+              }>
+              <Text style={styles.msgName}>
+                {msg.senderID === user.userid ? PERSON_NAME : BOT_NAME}
+              </Text>
+              <Text style={styles.msgText}>{msg.message}</Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.inputArea}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your message..."
+          value={input}
+          onChangeText={setInput}
+        />
+        <Button title="Send" onPress={handleSubmit} />
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f7fa',
+    padding: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#eee',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  chat: {
+    flex: 1,
+    padding: 10,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'grey',
+    marginRight: 10,
+  },
+  leftMsg: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginBottom: 10,
+  },
+  rightMsg: {
+    flexDirection: 'row-reverse',
+    alignItems: 'flex-end',
+    marginBottom: 10,
+  },
+  msgImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  leftBubble: {
+    maxWidth: '70%',
+    padding: 15,
+    borderRadius: 15,
+    backgroundColor: '#ececec',
+  },
+  rightBubble: {
+    maxWidth: '70%',
+    padding: 15,
+    borderRadius: 15,
+    backgroundColor: '#579ffb',
+  },
+  msgName: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  msgText: {
+    fontSize: 16,
+  },
+  inputArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#eee',
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#ddd',
+  },
+});
 
 export default ChatPageGroup;
